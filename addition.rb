56959be0793a805ea2da85ee2b474421e51bdc96
root <root@201011JUNC0384.(none)>
2015-03-05 15:08:39 +0530

    remove unwanted line

--- addition.rb
+++ addition.rb
@@ -2,5 +2,6 @@ puts 4 + 3
 puts 7 + 9
 puts first_name = "vempati jhansy"
 puts favotite_coloy = "black"
-puts last_name = "janu"
-gets last_name
+puts "hi, my name first_name.My fav color is fav_color"
+
+puts "hi,my first_name"
